595485209c77f5633dbe7c9b2e1392ec40925096
adsonluis1 <dev.adsonluis@gmail.com>
2024-06-26 17:12:51 -0300

    usando modulos internos

---
+++ 2_fundamentos/1_modulosInternos/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require('./modulo')
+const {somar,diminuir,multiplicar,dividir} = meuModulo
+
+somar(1,5)
+diminuir(10,5)
+multiplicar(5,4)
+dividir(4,2)
---
+++ 2_fundamentos/1_modulosInternos/modulo.js
@@ -0,0 +1,14 @@
+module.exports = {
+    somar (a,b){
+        console.log('somar ' + a+b)
+    },
+    diminuir (a,b){
+        console.log(`diminuir ${a-b}`)
+    },
+    multiplicar (a,b){
+        console.log('multiplicar ' + a*b)
+    },
+    dividir (a,b){
+        console.log('dividir ' + a/b)
+    }
+}
