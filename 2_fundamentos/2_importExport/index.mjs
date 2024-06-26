796ddeec46cd892dda81bb12ed1b6ae3e91739ae
adsonluis1 <dev.adsonluis@gmail.com>
2024-06-26 17:13:27 -0300

    Usando import e export com os modulos"

---
+++ 2_fundamentos/2_importExport/index.mjs
@@ -0,0 +1,6 @@
+import { somar, diminuir,multiplicar,dividir } from "../1_modulosInternos/modulo";
+
+somar(1,5)
+diminuir(10,5)
+multiplicar(5,4)
+dividir(4,2)
---
+++ 2_fundamentos/2_importExport/modulo.mjs
@@ -0,0 +1,13 @@
+export function somar (a,b){
+    console.log('somar ' + a+b)
+}
+export function diminuir (a,b){
+    console.log(`diminuir ${a-b}`)
+}
+
+export function multiplicar (a,b){
+    console.log('multiplicar ' + a*b)
+}
+export function dividir (a,b){
+    console.log('dividir ' + a/b)
+}
