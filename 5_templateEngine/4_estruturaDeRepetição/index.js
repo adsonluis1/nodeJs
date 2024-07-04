66a08b24e8e7fba10bac6c76702ed25c9919c504
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-03 22:07:42 -0300

    loops em handlebars

---
+++ 5_templateEngine/4_estruturaDeRepetição/index.js
@@ -0,0 +1,62 @@
+const engine = require('express-handlebars').engine
+const express = require('express')
+const app = express()
+
+
+
+function filterAcceptCnh (arr){
+    let auth = []
+    if(typeof arr == "object"){
+        arr.filter((pessoa)=>{
+        if(pessoa.idade > 17 && pessoa.cnh == false){
+            auth.push(pessoa)
+        }
+    })}
+    else if (arr.idade > 17 && arr.cnh == false){
+        auth = arr
+    }
+    return auth
+}
+
+app.engine('handlebars',engine())
+app.set('view engine','handlebars')
+app.set('views','./views')
+express.static('public')
+
+app.get('/',(req, res)=>{
+    let users = [
+        ,{
+            nome:'adson',
+            idade:19,
+            cnh:true
+        }
+        ,{
+            nome:'luis',
+            idade:23,
+            cnh:false
+        }
+        ,{
+            nome:'Roberta',
+            idade:17,
+            cnh:false
+        }
+        ,{
+            nome:'Rafaelle',
+            idade:24,
+            cnh:false
+        }
+        ,{
+            nome:'anne',
+            idade:16,
+            cnh:true
+        }
+    ]
+
+    users = filterAcceptCnh(users)
+
+    res.render('home',{users})
+})
+
+app.listen(3333,()=>{
+    console.log('rodando')
+})
