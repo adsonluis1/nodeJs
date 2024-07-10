5d37a4bf90cf06c2af721b561a2382e9c2bfe1ee
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-09 22:02:01 -0300

    melhorando a performance com cash

---
+++ 6_nodeMysql/7_connectionPool/index.js
@@ -0,0 +1,129 @@
+const pool = require('./db/conn')
+const handlebars = require('express-handlebars')
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded(
+    {extended: true}
+))
+
+app.use(express.json())
+
+const newEngine = handlebars.create([
+    {partialsDir:'views/partials'}
+])
+
+app.engine('handlebars', newEngine.engine)
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+
+app.get('/',(req, res)=> {
+    res.render('home')
+})
+
+app.get('/books',(req, res)=>{
+    const search = "SELECT * FROM books"
+    let books
+    pool.query(search,(err,data)=>{
+        if(err){
+            console.log(err)
+            res.redirect('/')
+            return
+        }
+
+        books = data
+        res.render('showBooks', {books})
+    })
+
+})
+
+app.get('/books/:id',(req, res)=>{
+    const idBook = req.params.id
+    const search = `SELECT * FROM books WHERE id = ${idBook}`
+    let book
+    let auth = true 
+    pool.query(search,(err, data)=>{
+        if(err){
+            console.log(err)
+            res.redirect('/')
+            return
+        }
+        book = data[0]
+        if(book == undefined){
+            auth = false
+        }
+        book = data[0]
+        res.render('showBook',{book,auth})
+    })
+    
+})
+
+app.get('/books/edit/:id',(req, res)=>{
+    const idBook = req.params.id
+    const search = `SELECT * FROM books WHERE id = ${idBook}`
+    let book
+    let auth = true 
+    pool.query(search,(err, data)=>{
+        if(err){
+            console.log(err)
+            res.redirect('/')
+            return
+        }
+        book = data[0]
+        if(book == undefined){
+            auth = false
+        }
+        book = data[0]
+        res.render('editBook',{book,auth})
+    })
+})
+
+app.get('/books/remove/:id',(req, res)=>{
+    const idBook = req.params.id
+    
+    const sql = `DELETE FROM books WHERE id=${idBook}`
+    
+    pool.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        
+        res.redirect('/books')
+    })
+})
+
+app.post('/books/add',(req, res)=>{
+    const {title, pagesqty} = req.body
+    
+    const sql = `INSERT INTO books (title, pagesqty) VALUES ('${title}', '${pagesqty}')`
+    
+    pool.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        
+        res.redirect('/books')
+    })
+})
+
+app.post('/book/edit/add',(req, res)=>{
+    const {id, title, pagesqty} = req.body
+
+    const sql = `UPDATE books SET title='${title}', pagesqty='${pagesqty}' WHERE id=${id}`
+
+    pool.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        
+        res.redirect('/books')
+    })
+
+})
+
+app.listen(3333,()=>{
+    console.log('rodando')
+})
