ef3a47947aaff5acca8ac201a44df6719aaced0e
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-15 22:46:22 -0300

    rota dashbord

--- 8_toughts/routes/userRouter.js
+++ 8_toughts/routes/userRouter.js
@@ -1,10 +1,22 @@
-const expres = require('express')
-const router = expres.Router()
+const express = require('express')
+const router = express.Router()
+const checkAuth = require('../helpers/donepage').checkAuth
 const UsersControlls = require('../controllers/UsersControlls')
 
-router.get('/register', UsersControlls.formCreateAccount)
+router.use((req,res,next) => {
+
+    if(req.session.account){
+        res.locals.session = req.session
+    }
+
+    next()
+})
+
+router.get('/register',UsersControlls.formCreateAccount)
 router.get('/login', UsersControlls.formLoginAccount)
-router.post('/create', UsersControlls.createAccount)
+router.get('/logout', UsersControlls.logoutAccount)
+router.get('/dashbord', checkAuth, UsersControlls.dashbord)
+router.post('/register', UsersControlls.createAccount)
 router.post('/login', UsersControlls.loginAccount)
 
 module.exports = router
