b058722c4ac5f4b14dd0f79d1d5a0a94e7cc4ef8
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-16 12:27:46 -0300

    Ajeitando o middleware

--- 8_toughts/helpers/donepage.js
+++ 8_toughts/helpers/donepage.js
@@ -1,9 +1,9 @@
 module.exports.checkAuth = function (req, res, next) {
     const account = req.session.account
-  
     if (!account || account == undefined) {
       res.redirect('/user/login')
+      return
     }
-  
+    
     next()
   }
