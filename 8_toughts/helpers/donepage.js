35ee5475e119660da83ca8101ae59dcae0004f5b
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-15 22:40:51 -0300

    verificando se o usuario pode acessar a pagina

---
+++ 8_toughts/helpers/donepage.js
@@ -0,0 +1,9 @@
+module.exports.checkAuth = function (req, res, next) {
+    const account = req.session.account
+  
+    if (!account || account == undefined) {
+      res.redirect('/user/login')
+    }
+  
+    next()
+  }
