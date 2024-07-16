38c1552ab234878631d5e76974240689dc8d8e46
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-15 22:43:54 -0300

    rotas de add, edit, delete e forms

---
+++ 8_toughts/routes/thoughtRourter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const {checkAuth} = require('../helpers/donepage')
+const ThoughtControllers = require('../controllers/ThoughtControllers')
+
+router.use((req,res,next) => {
+
+    if(req.session.account){
+        res.locals.session = req.session
+    }
+
+    next()
+})
+
+router.get('/add',checkAuth, ThoughtControllers.createFormThought)
+router.get('/delete/:id', checkAuth, ThoughtControllers.deleteThought)
+router.get('/edit/:id', checkAuth, ThoughtControllers.editFormThought)
+router.post('/edit', checkAuth, ThoughtControllers.editThought)
+router.post('/add', checkAuth, ThoughtControllers.addThought)
+
+module.exports = router
