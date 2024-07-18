0d08aa8cdb8cc718a9cbc163973dd16bfdfe96ed
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-18 10:47:39 -0300

    adicionado funcionalidades de delete e edit e fazendo a render do form edit

--- 9_nodeMongodb/1_init/controllers/ProdutosControllers.js
+++ 9_nodeMongodb/1_init/controllers/ProdutosControllers.js
@@ -9,6 +9,30 @@ module.exports = class ProdutosControllers {
     static formCreate (req, res){
         res.render('produtos/formCreate')
     }
+    
+    static async formEdit (req, res){
+        const {id} = req.params
+        const product = await Produtos.showProductById(id)
+        console.log(product)
+        res.render('produtos/formEdit', {product})
+    }
+
+    static async productsDelete (req, res){
+        const {id} = req.params
+
+        await Produtos.delete(id)
+        res.redirect('/')
+    }
+
+    static async productsEdit (req, res){
+        const {id} = req.params
+        const {name, price, description} = req.body
+
+        const produto = new Produtos(name,price,description)
+
+        await produto.edit(id)
+        res.redirect('/')
+    }
 
     static productsCreate (req, res){
         const {name, price, description} = req.body
