f04d78fed7ddbe22b3487d6a1e9a72070de47617
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-18 12:21:07 -0300

    primeiro push em api

--- 10_nodeAPI/1_init/index.js
+++ 10_nodeAPI/1_init/index.js
@@ -6,7 +6,23 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.get('/',(req, res)=>{
-    res.json({menssage:'hello world'})
+    res.status(200).json({message:'hello world'})
+})
+
+app.post('/createproduct', (req, res)=>{
+    const {name, price} = req.body
+    
+    if(!name){
+        res.status(422).json({message:"name is input required"})
+        return
+    }
+
+    if(!price){
+        res.status(422).json({message:"price is input required"})
+        return
+    }
+
+    res.status(201).json({message:`O produto ${name} foi criado com sucesso`})
 })
 
 app.listen(3333)
