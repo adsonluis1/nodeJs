72df41fca5fa275793aa91a3f63832510b8c4a32
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-18 10:48:36 -0300

    delete, edit e pegar o produto pelo id

--- 9_nodeMongodb/1_init/models/produtos.js
+++ 9_nodeMongodb/1_init/models/produtos.js
@@ -1,4 +1,5 @@
 const conn = require('../db/conn')
+const {ObjectId} = require('mongodb')
 
 module.exports = class Product {
     constructor(name, price, description){
@@ -7,13 +8,30 @@ module.exports = class Product {
         this.description = description
     }
 
-    static showProducts (){
+    static  showProducts (){
         const products = conn.db().collection('products').find().toArray()
        return products
     }
 
-    save(){
-        const product = conn.db().collection('products').insertOne({
+    static async showProductById (productId){
+        const product = await conn.db().collection('products').findOne({_id:new ObjectId(productId)})
+        return product
+    }
+
+    static async delete(productId){
+        await conn.db().collection('products').deleteOne({_id:new ObjectId(productId)})
+    }
+
+    async edit(productId){
+        await conn.db().collection('products').updateOne({_id:new ObjectId(productId)}, {$set:{
+            name:this.name,
+            price:this.price,
+            description:this.description 
+        }})
+    }
+
+    async save(){
+        const product = await conn.db().collection('products').insertOne({
             name:this.name,
             price:this.price,
             description:this.description
