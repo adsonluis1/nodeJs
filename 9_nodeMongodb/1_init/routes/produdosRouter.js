82af6e8b948705454422fdd1ff2822efb6c0d80e
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-18 10:50:04 -0300

    rotas de delete e edit

--- 9_nodeMongodb/1_init/routes/produdosRouter.js
+++ 9_nodeMongodb/1_init/routes/produdosRouter.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const ProdutosControllers = require('../controllers/ProdutosControllers')
 
 router.get('/create', ProdutosControllers.formCreate)
+router.get('/delete/:id', ProdutosControllers.productsDelete)
+router.get('/edit/:id', ProdutosControllers.formEdit)
+router.post('/edit/:id', ProdutosControllers.productsEdit)
 router.post('/create', ProdutosControllers.productsCreate)
 
 module.exports = router
