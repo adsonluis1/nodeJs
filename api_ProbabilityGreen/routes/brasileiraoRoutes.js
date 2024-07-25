4e6df54d29c3b85c0348d56416cbfa94dff7c6ea
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-24 22:33:32 -0300

    rota para pegar os proximos jogos e rota para excluir os jogos que já acabaram

--- api_ProbabilityGreen/routes/brasileiraoRoutes.js
+++ api_ProbabilityGreen/routes/brasileiraoRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 const brasileiraoA = require('../controllers/brasileirao')
 const router = express.Router()
 
+router.get('/proximosJogos', brasileiraoA.getGamesByProximosJogosCampeonato)
 router.post('/addTime', brasileiraoA.addTime)
 router.patch('/changingStatistics', brasileiraoA.changingTeamStatistics)
 router.patch('/changingProximosJogos', brasileiraoA.changingTeamStatisticsProximosJogos)
 router.patch('/patchTable', brasileiraoA.patchTable)
+router.patch('/deleteProximosJogos', brasileiraoA.removeGamesProximosJogosCampeonatoByTime)
 
 module.exports = router
