9265e594136b1c1bfe4e56192ed9e6e4b8f4005c
adsonluis1 <dev.adsonluis@gmail.com>
2024-07-29 23:20:37 -0300

    add rota de delete

--- api_ProbabilityGreen/routes/brasileiraoRoutes.js
+++ api_ProbabilityGreen/routes/brasileiraoRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const brasileiraoA = require('../controllers/brasileirao')
 const router = express.Router()
 
+router.get('/', brasileiraoA.getTable)
 router.get('/proximosJogos', brasileiraoA.getGamesByProximosJogosCampeonato)
 router.get(/[a-z]+/i, brasileiraoA.getTimeByName)
 router.post('/addTime', brasileiraoA.addTime)
@@ -9,6 +10,6 @@ router.post('/addProximosJogos', brasileiraoA.addGamesInProximosJogos)
 router.patch('/changingStatistics', brasileiraoA.changingTeamStatistics)
 router.patch('/changingProximosJogos', brasileiraoA.changingTeamStatisticsProximosJogos)
 router.patch('/patchTable', brasileiraoA.patchTable)
-router.patch('/deleteProximosJogos', brasileiraoA.removeGamesProximosJogosCampeonatoByTime)
+router.delete('/deleteProximosJogos', brasileiraoA.removeGamesProximosJogosCampeonatoByTime)
 
 module.exports = router
